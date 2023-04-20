2f8fec48acbac77e50ce336f141e5af20b57f20e
Artur <arturli.official@gmail.com>
2023-04-20 09:35:15 -0400

    all steps completed (except for color picker and slider)

--- main.py
+++ main.py
@@ -17,6 +17,31 @@ password_field.send_keys("12345678")
 text_field = driver.find_element(By.NAME, "my-textarea")
 text_field.send_keys("typed by script!")
 
+select_element = driver.find_element(By.NAME, "my-select")
+dropdown_menu1 = driver.find_element(By.NAME, "my-select")
+dropdown_menu1.click()
+options = driver.find_elements(By.TAG_NAME, "option")
+for option in options:
+    if option.text == "One":
+        option.click()
+        dropdown_menu1.click()
+
+dropdown_menu2 = driver.find_element(By.NAME, "my-datalist")
+dropdown_menu2.send_keys("San Francisco")
+
+box1 = driver.find_element(By.ID, "my-check-1")
+box1.click()
+box2 = driver.find_element(By.ID, "my-check-2")
+box2.click()
+select_radio = driver.find_element(By.ID, "my-radio-2")
+select_radio.click()
+
+date = driver.find_element(By.NAME, "my-date")
+date.send_keys("10/10/2023")
+
+submit = driver.find_element(By.TAG_NAME, "button")
+submit.click()
+
 # keep open for 10 seconds
 time.sleep(10)
 
