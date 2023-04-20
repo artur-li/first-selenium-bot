33d8ef8257ac882149d3b346d1c0e50a0ffc6b45
Artur <arturli.official@gmail.com>
2023-04-20 08:46:28 -0400

    locates and adds text in website

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
 import time
 
 # initialize chrome webdriver
@@ -6,6 +7,15 @@ driver = Chrome("C://Users//liart//Downloads//chromedriver_win32//chromedriver.e
 
 # open this url
 driver.get("https://www.selenium.dev/selenium/web/web-form.html")
+
+username_field = driver.find_element(By.ID, "my-text-id")
+username_field.send_keys("Arturo")
+
+password_field = driver.find_element(By.NAME, "my-password")
+password_field.send_keys("12345678")
+
+text_field = driver.find_element(By.NAME, "my-textarea")
+text_field.send_keys("typed by script!")
 
 # keep open for 10 seconds
 time.sleep(10)
